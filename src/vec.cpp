adeea8bc0d4f4a2a71fdc344b56c6300e3873878
Willy Huang <breezekiller789@csie.io>
2020-03-25 15:34:37 +0800

    3/25

--- src/vec.cpp
+++ src/vec.cpp
@@ -32,7 +32,7 @@ int main(int argc, char *argv[]) {
     /* } */
 
     sort(vec.begin(), vec.end());
-    for(int i=0; i<vec.size(); i++){
+    for(unsigned int i=0; i<vec.size(); i++){
         cout << vec[i] << endl;
     }
 
