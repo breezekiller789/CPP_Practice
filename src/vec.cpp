749df09eeeee1affe203cbff579274786266bc4f
Willy Huang <breezekiller789@csie.io>
2020-03-25 15:07:49 +0800

    3/25

--- src/vec.cpp
+++ src/vec.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include <vector>
 #include "header.h"
+#include <algorithm>
+
 
 using namespace std;
 
-void Parse_Input(FILE *fp, vector<int> vec){
+void Parse_Input(FILE *fp, vector<int> *vec){
     
     int num, cnt=0;
     while((fscanf(fp, "%d", &num)) != EOF){
-        printf("%d\n", num);
+        vec->push_back(num);
+        /* printf("%d\n", num); */
         cnt++;
     }
-    printf("cnt = %d\n", cnt);
-
 }
 
 int main(int argc, char *argv[]) {
@@ -23,7 +24,17 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    Parse_Input(fp, vec);
+    Parse_Input(fp, &vec);
+
+    printf("size = %lu\n", vec.size());
+    /* for(int i=0; i<vec.size(); i++){ */
+    /*     cout << vec[i] << endl; */
+    /* } */
+
+    sort(vec.begin(), vec.end());
+    for(int i=0; i<vec.size(); i++){
+        cout << vec[i] << endl;
+    }
 
     return 0;
 }
